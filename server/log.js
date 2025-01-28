313f4c18facd61e64c51b6ad15d14828562b0fb6
Lars Brandt <zebrajaeger@zebrajaeger.de>
2025-01-28 10:11:07 +0100

    [fix] missing import

--- server/log.js
+++ server/log.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
 
 module.exports.log = log;
 // Logging-Funktion
 function log(message) {
     const timestamp = new Date().toISOString();
     const logMessage = `[${timestamp}] ${message}\n`;
-    fsSync.appendFileSync('./server.log', logMessage);
+    fs.appendFileSync('./server.log', logMessage);
     console.log(logMessage.trim());
   }
